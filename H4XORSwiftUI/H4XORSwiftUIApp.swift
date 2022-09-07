5366a272d092df790fc34ba65211c289a830fce9
Muhammed YILDIRIM <muhammedyildirim48@gmail.com>
2022-09-08 01:41:24 +0300

    Initial Commit

---
+++ H4XORSwiftUI/H4XORSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XORSwiftUIApp.swift
+//  H4XORSwiftUI
+//
+//  Created by YILDIRIM on 8.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct H4XORSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
